96fc1587f3ac4ad72a7b6043b5874799fd91db84
André Kovac <me@andrekovac.com>
2019-12-18 01:11:59 +0100

    iOS: Add init function and make functions safe concerning missing parameters

--- ios/HelpscoutBeacon.m
+++ ios/HelpscoutBeacon.m
@@ -16,16 +16,52 @@ RCT_EXPORT_MODULE();
     return dispatch_get_main_queue();
 }
 
-NSString * const beaconID = @"***REMOVED***";
+NSString *helpscoutBeaconID;
+
+RCT_EXPORT_METHOD(init:(NSString *)beaconID)
+{
+    if (beaconID == nil) {
+        NSLog(@"[init] missing parameter: beaconID");
+        return;
+    }
+
+    helpscoutBeaconID = beaconID;
+}
 
 RCT_EXPORT_METHOD(open:(NSString *)signatureKey)
 {
-    HSBeaconSettings *settings = [[HSBeaconSettings alloc] initWithBeaconId:beaconID];
+    if (helpscoutBeaconID == nil || signatureKey == nil) {
+        if (helpscoutBeaconID == nil) {
+            NSLog(@"[open] Not initialized - did you forget to call 'init'?");
+        }
+        if (signatureKey == nil) {
+            NSLog(@"[open] missing parameter: signatureKey");
+        }
+        return;
+    }
+
+    HSBeaconSettings *settings = [[HSBeaconSettings alloc] initWithBeaconId:helpscoutBeaconID];
     [HSBeacon openBeacon:settings signature:signatureKey];
 }
 
 RCT_EXPORT_METHOD(login:(NSString *)email nameParameter:(NSString *)name userIdParameter:(NSString *)userID)
 {
+    if (helpscoutBeaconID == nil || email == nil || name == nil || userID == nil) {
+        if (helpscoutBeaconID == nil) {
+            NSLog(@"[login] Not initialized - did you forget to call 'init'?");
+        }
+        if (email == nil) {
+            NSLog(@"[login] missing parameter: email");
+        }
+        if (name == nil) {
+            NSLog(@"[login] missing parameter: name");
+        }
+        if (userID == nil) {
+            NSLog(@"[login] missing parameter: userID");
+        }
+        return;
+    }
+
     HSBeaconUser *user = [[HSBeaconUser alloc] init];
     user.email = email;
     user.name = name;
@@ -35,6 +71,25 @@ RCT_EXPORT_METHOD(login:(NSString *)email nameParameter:(NSString *)name userIdP
 
 RCT_EXPORT_METHOD(loginAndOpen:(NSString *)email nameParameter:(NSString *)name userIdParameter:(NSString *)userID signatureParameter:(NSString *)signature)
 {
+    if (helpscoutBeaconID == nil || email == nil || name == nil || userID == nil) {
+        if (helpscoutBeaconID == nil) {
+            NSLog(@"[loginAndOpen] Not initialized - did you forget to call 'init'?");
+        }
+        if (email == nil) {
+            NSLog(@"[loginAndOpen] missing parameter: email");
+        }
+        if (name == nil) {
+            NSLog(@"[loginAndOpen] missing parameter: name");
+        }
+        if (userID == nil) {
+            NSLog(@"[loginAndOpen] missing parameter: userID");
+        }
+        if (signature == nil) {
+            NSLog(@"[loginAndOpen] missing parameter: signature");
+        }
+        return;
+    }
+
     [self login:email nameParameter:name userIdParameter:userID];
     [self open:signature];
 }
@@ -46,12 +101,35 @@ RCT_EXPORT_METHOD(logout)
 
 RCT_EXPORT_METHOD(openArticle:(NSString *)articleID signatureParameter:(NSString *)signature)
 {
-    HSBeaconSettings *settings = [[HSBeaconSettings alloc] initWithBeaconId:beaconID];
+    if (helpscoutBeaconID == nil || articleID == nil || signature == nil) {
+        if (helpscoutBeaconID == nil) {
+            NSLog(@"[openArticle] Not initialized - did you forget to call 'init'?");
+        }
+        if (articleID == nil) {
+            NSLog(@"[openArticle] missing parameter: articleID");
+        }
+        if (signature == nil) {
+            NSLog(@"[openArticle] missing parameter: signature");
+        }
+        return;
+    }
+
+    HSBeaconSettings *settings = [[HSBeaconSettings alloc] initWithBeaconId:helpscoutBeaconID];
     [HSBeacon openArticle:articleID beaconSettings: settings signature:signature];
 }
 
 RCT_EXPORT_METHOD(suggestArticles:(NSArray *)articleIDList)
 {
+    if (helpscoutBeaconID == nil || articleIDList == nil) {
+        if (helpscoutBeaconID == nil) {
+            NSLog(@"[suggestArticles] Not initialized - did you forget to call 'init'?");
+        }
+        if (articleIDList == nil) {
+            NSLog(@"[suggestArticles] missing parameter: articleIDList");
+        }
+        return;
+    }
+
     [HSBeacon suggest:articleIDList];
 }
 
